ba34474c3599b85fa254ce7bd9c679da12fa3ed6
Steven Burton <steven.burton1@dwp.gov.uk>
2019-07-25 11:19:56 +0100

    DW-2447 Add new lamdba code

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # hbase-retriever
-Retrieves records from HBase
+
+A simple Lambda to allow clients to retrieve values from Hbase in EMR that
+have been written by Kafka2Hbase (see https://github.com/dwp/kafka-to-hbase).
+
+To request a record, call the lambda with a document containing a topic and
+message key from Kafka. The value of that message as persisted in Hbase
+will be returned.
+
+```json
+{
+  "topic": "db.claimants.address",
+  "key": "1c2c47d6-5b8b-4d82-929e-b5b05b1b2306"
+}
+```
+
+If a specific version (other than "latest") is required, specify the
+version as a numeric timestamp.
+
+```json
+{
+  "topic": "db.claimants.address",
+  "key": "1c2c47d6-5b8b-4d82-929e-b5b05b1b2306",
+  "timestamp": 1563809926831
+}
+```
+
+The returned value is the raw bytes as received from Kafka. If the
+integration response is set to convert to text, it will be a base64 encoded
+string.
+
+## Configuration
+
+Configuration of the Lambda is done entirely via environment variables.
+
+* **ZOOKEEPER_QUORUM** - set `hbase.zookeeper.quorum` to a list of hosts (default `zookeeper`)
+* **ZOOKEEPER_PORT** - set `hbase.zookeeper.port` to an integer port (default `2181`)
+* **TABLE** - the fully qualified Hbase table to read from
+* **FAMILY** - the column family to search for
+
+Rows are retrieved from the configured table using the requested key. The
+cell value is fetched from the configured column family using the requested
+topic as the qualifier. If a timestamp is specified, the matching version is returned.
+
+If no valid row is found, null is returned.
---
+++ src/main/kotlin/Handler.kt
@@ -0,0 +1,45 @@
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.TableName
+import org.apache.hadoop.hbase.client.ConnectionFactory
+import org.apache.hadoop.hbase.client.Get
+import org.apache.log4j.ConsoleAppender
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.log4j.PatternLayout
+
+@Suppress("unused")
+class Handler : RequestHandler<Request, ByteArray?> {
+    init {
+        val consoleAppender = ConsoleAppender()
+        consoleAppender.layout = PatternLayout("%d [%p|%c|%C{1}] %m%n")
+        consoleAppender.threshold = Level.INFO
+        consoleAppender.activateOptions()
+        Logger.getRootLogger().addAppender(consoleAppender)
+    }
+
+    override fun handleRequest(input: Request, context: Context?): ByteArray? {
+        val topic = input.topic.toByteArray()
+        val key = input.key.toByteArray()
+        val timestamp = input.timestamp
+        val family = HbaseConfig.family.toByteArray()
+
+        // Connect to Hbase using configured values
+        val connection = ConnectionFactory.createConnection(HBaseConfiguration.create(HbaseConfig.config))
+
+        // Get the requested record with an optional version number
+        connection.getTable(TableName.valueOf(HbaseConfig.table)).use { table ->
+            val result = table.get(Get(key).apply {
+                if (input.timestamp > 0) {
+                    setTimeStamp(timestamp)
+                }
+
+                addColumn(family, topic)
+            })
+
+            // Return the value of the cell directly, parsed as a UTF-8 string
+            return result.getValue(family, topic)
+        }
+    }
+}
