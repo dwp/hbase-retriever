ff20ca570d15702ff1b90b981a4e65013e6c8f62
Ben F <benj-fletch@users.noreply.github.com>
2020-04-27 15:24:29 +0100

    Upversion logging library to latest 0.0.5 (#31)

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     implementation("com.beust", "klaxon", "4.0.2")
     implementation("junit:junit:4.12")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.0")
-    implementation("com.github.dwp:dataworks-common-logging:0.0.4")
+    implementation("com.github.dwp:dataworks-common-logging:0.0.5")
     testImplementation("io.kotlintest", "kotlintest-runner-junit5", "3.3.2")
     runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
     runtimeOnly("ch.qos.logback:logback-core:1.2.3")
