5152ce51906a843b8d5bbdf28c784b90f60342d6
steveburton4 <steven.burton@infinityworks.com>
2020-04-27 19:45:37 +0100

    Suppress log4j for custom logger is used

--- src/test/kotlin/KeyGenerationTest.kt
+++ src/test/kotlin/KeyGenerationTest.kt
@@ -7,6 +7,8 @@ import io.kotlintest.matchers.beInstanceOf
 import io.kotlintest.specs.StringSpec
 import com.beust.klaxon.JsonObject
 import org.junit.Test
+import io.mockk.every
+import io.mockk.mockkObject
 import org.hamcrest.CoreMatchers.instanceOf;
 
 
