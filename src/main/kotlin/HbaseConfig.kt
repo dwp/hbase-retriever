0bcbb89348ee403224e9d13261f3a29c1544ce7f
steveburton4 <Steven.burton@hotmail.co.uk>
2020-04-29 20:40:10 +0100

    Add hbase scanner restrictions (#35)
    * Retrict the HBase cache to 1 as we will never need more than one row
    
    * Check table is not enabled when deleting
    
    * Missed the null check
    
    Co-authored-by: steveburton4 <steven.burton@infinityworks.com>

--- src/main/kotlin/HbaseConfig.kt
+++ src/main/kotlin/HbaseConfig.kt
@@ -6,6 +6,8 @@ object HbaseConfig {
         set("zookeeper.znode.parent", getEnv("ZOOKEEPER_PARENT") ?: "/hbase")
         set("hbase.zookeeper.quorum", getEnv("ZOOKEEPER_QUORUM") ?: "zookeeper")
         setInt("hbase.zookeeper.port", getEnv("ZOOKEEPER_PORT")?.toIntOrNull() ?: 2181)
+        setInt("hbase.client.scanner.timeout.period", getEnv("HBASE_CLIENT_SCANNER_TIMEOUT_PERIOD")?.toIntOrNull() ?: 60000)
+        setInt("hbase.client.scanner.caching", getEnv("HBASE_CLIENT_SCANNER_SCANNER_CACHING")?.toIntOrNull() ?: 1)
     }
 
     val dataFamily = getEnv("FAMILY") ?: "cf"
